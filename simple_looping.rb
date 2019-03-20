edcfe4340592072eb34d958dd29446aa31a7c60c
Susan Tan <ssusantan9@gmail.com>
2019-03-20 04:22:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,6 @@ def loop_iterator(number_of_times)
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
     break if number_of_times > 7
-    number_of_times += 1
   end
 end
 
