f5fa40a4ef4d70f11f1346629a10e3ace652232a
Susan Tan <ssusantan9@gmail.com>
2019-03-20 04:15:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,32 +8,32 @@ def loop_iterator(number_of_times)
   end
 end
 
-def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+# def times_iterator(number_of_times)
+#   # code your solution here using the "times" keyword
+#   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-end
+# end
 
-def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+# def while_iterator(number_of_times)
+#   # code your solution here using the "while" keyword
+#   # hint: user a counter to tell the while loop when to stop!
+#   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-end
+# end
 
-def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+# def until_iterator(number_of_times)
+#   # code your solution here using the "until" keyword
+#   # hint: use a counter to tell the until loop when to stop!
+#   phrase = "Welcome to Flatiron School's Web Development Course!"
 
-end
+# end
 
-def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
+# def for_iterator(number_of_times)
+#   # code your solution here using the "for" keyword
+#   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+#   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-end
+# end
 
 
