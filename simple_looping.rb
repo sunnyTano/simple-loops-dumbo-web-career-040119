2691d6612e13499cc23cfd76126ad0b1b9f4a64c
Susan Tan <ssusantan9@gmail.com>
2019-03-20 04:28:15 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -17,9 +17,9 @@ end
 
 def while_iterator(number_of_times)
   number_of_times = 0
- while number_of_times <= 7
-   number_of_times += 1
+ while number_of_times < 7
   puts "Welcome to Flatiron School's Web Development Course!"
+    number_of_times += 1
   end
 end
 
