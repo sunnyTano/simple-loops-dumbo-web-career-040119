09676880bb13d69ae9daa9cf6ed9c2b2fd87682c
Susan Tan <ssusantan9@gmail.com>
2019-03-20 04:32:47 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -26,8 +26,8 @@ end
 def until_iterator(number_of_times)
   number_of_times == 0 
     until number_of_times === 7
-    number_of_times += 1
       puts "Welcome to Flatiron School's Web Development Course!"
+       number_of_times += 1
   end
 end
 
